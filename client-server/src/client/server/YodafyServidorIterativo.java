f7c36f6097ae1aed72b34512c360cff7cb3833e0
rafaeling <rafaelingugr@gmail.com>
2015-11-27 16:49:10 +0100

    Iterative Server

--- client-server/src/client/server/YodafyServidorIterativo.java
+++ client-server/src/client/server/YodafyServidorIterativo.java
@@ -23,19 +23,25 @@ public class YodafyServidorIterativo {
 
 	public static void main(String[] args) {
 	
-		// Puerto de escucha
-		int port=8989;
-		// array de bytes auxiliar para recibir o enviar datos.
-		byte []buffer=new byte[256];
-		// Número de bytes leídos
-		int bytesLeidos=0;
-		
-		try {
+            // Puerto de escucha
+            int port=888;
+            // array de bytes auxiliar para recibir o enviar datos.
+            byte []buffer=new byte[256];
+            // Número de bytes leídos
+            
+            int bytesLeidos=0;
+            
+            ServerSocket serverSocket;
+            Socket socketServicio;
+            
+                
+            try {
 			// Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
 			//////////////////////////////////////////////////
 			// ...serverSocket=... (completar)
 			//////////////////////////////////////////////////
-			
+			 serverSocket = new ServerSocket(port);
+                        
 			// Mientras ... siempre!
 			do {
 				
@@ -44,17 +50,20 @@ public class YodafyServidorIterativo {
 				// socketServicio=... (completar)
 				//////////////////////////////////////////////////
 				
+                                socketServicio = serverSocket.accept();
+                                
 				// Creamos un objeto de la clase ProcesadorYodafy, pasándole como 
 				// argumento el nuevo socket, para que realice el procesamiento
 				// Este esquema permite que se puedan usar hebras más fácilmente.
 				ProcesadorYodafy procesador=new ProcesadorYodafy(socketServicio);
 				procesador.procesa();
+                                
 				
 			} while (true);
 			
 		} catch (IOException e) {
-			System.err.println("Error al escuchar en el puerto "+port);
-		}
+                System.out.println("Error: no se pudo atender en el puerto "+port);
+                }
 
 	}
 
