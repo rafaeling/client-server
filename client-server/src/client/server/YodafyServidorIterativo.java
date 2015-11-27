7341acb0b7199af657d0b8e26bdb442716b6ff7a
rafaeling <rafaelingugr@gmail.com>
2015-11-27 17:17:28 +0100

    Server

--- client-server/src/client/server/YodafyServidorIterativo.java
+++ client-server/src/client/server/YodafyServidorIterativo.java
@@ -7,7 +7,7 @@ package client.server;
 
 /**
  *
- * @author rafae
+ * @author rafaeling
  */
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,54 +15,50 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-//
-// YodafyServidorIterativo
-// (CC) jjramos, 2012
-//
+
 public class YodafyServidorIterativo {
 
 	public static void main(String[] args) {
 	
             // Puerto de escucha
-            int port=888;
+            int port=8989;
+            
             // array de bytes auxiliar para recibir o enviar datos.
             byte []buffer=new byte[256];
-            // Número de bytes leídos
             
+            // Número de bytes leídos        
             int bytesLeidos=0;
             
+            // Servidor
             ServerSocket serverSocket;
+            
+            // Servicio
             Socket socketServicio;
             
                 
             try {
 			// Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
-			//////////////////////////////////////////////////
-			// ...serverSocket=... (completar)
-			//////////////////////////////////////////////////
 			 serverSocket = new ServerSocket(port);
                         
 			// Mientras ... siempre!
 			do {
 				
 				// Aceptamos una nueva conexión con accept()
-				/////////////////////////////////////////////////
-				// socketServicio=... (completar)
-				//////////////////////////////////////////////////
-				
                                 socketServicio = serverSocket.accept();
+                                
                                 
 				// Creamos un objeto de la clase ProcesadorYodafy, pasándole como 
 				// argumento el nuevo socket, para que realice el procesamiento
 				// Este esquema permite que se puedan usar hebras más fácilmente.
 				ProcesadorYodafy procesador=new ProcesadorYodafy(socketServicio);
+                                
 				procesador.procesa();
                                 
 				
-			} while (true);
+			}while(true);
 			
 		} catch (IOException e) {
-                System.out.println("Error: no se pudo atender en el puerto "+port);
+                    System.out.println("Error: no se pudo atender en el puerto " + port);
                 }
 
 	}
